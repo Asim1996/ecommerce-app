76720259a840844bbcedc47f80dee99ff48a8ce0
Asim1996 <awesomeasim2596@gmail.com>
2019-01-24 18:03:31 +0530

    implement ejs and changed folder structure

--- app.js
+++ app.js
@@ -5,17 +5,17 @@ const bodyParser = require('body-parser');
 
 const app = express();
 
+app.set('view engine','ejs');
+const errorController=require('./controllers/error');
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 
-app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 
-app.use((req, res, next) => {
-    res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
-});
+app.use(errorController.get404);
 
 app.listen(3000);
