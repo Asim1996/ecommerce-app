872f8d903b887517136ec7a3e8856a2f7e7a72be
Asim1996 <awesomeasim2596@gmail.com>
2019-03-11 22:33:22 +0530

    sql temp

--- app.js
+++ app.js
@@ -4,10 +4,14 @@ const express = require('express');
 const bodyParser = require('body-parser');
 
 const errorController = require('./controllers/error');
-const db = require('./util/database');
-
+const sequelize=require('./util/database');
 const app = express();
-
+const Product=require('./models/product');
+const User=require('./models/user');
+const Cart=require('./models/cart');
+const CartItem=require('./models/cart-item');
+const Order=require('./models/order');
+const OrderItem=require('./models/order-item');
 app.set('view engine', 'ejs');
 app.set('views', 'views');
 
@@ -18,9 +22,54 @@ const shopRoutes = require('./routes/shop');
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use((req,res,next)=>{
+    User.findById(1)
+    .then(user=>{
+        // console.log("check",user);
+        req.user=user;
+        next();
+    })
+    .catch(err=>{
+        console.log(err);
+    })
+})
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
-
+// Cart keeps tracks of every transaction
+//CartItem is a link between product and that cart
 app.use(errorController.get404);
+Product.belongsTo(User,{constraints:true,onDelete:'CASCADE'});
+User.hasMany(Product);//Optional
+User.hasOne(Cart);
+Cart.belongsTo(User);//Optional
+Cart.belongsToMany(Product,{through:CartItem});
+Product.belongsToMany(Cart,{through:CartItem});    
+//Order to User 1 to many
+Order.belongsTo(User);
+User.hasMany(Order);
+Order.belongsToMany(Product,{through:OrderItem});    
 
-app.listen(3000);
+sequelize
+.sync({force:true})
+// .sync()
+.then(result=>{
+    // console.log(result);
+    return User.findById(1);
+ }).then(user=>{
+     if(!user){
+        return User.create({
+             name:'asim',
+             email:'test@test.com'
+         })
+     }
+     return user;
+    //  return Promise.resolve(user);
+ }).then(user=>{
+     return user.createCart();
+ })
+ .then(cart=>{
+     app.listen(3000);
+ })
+.catch(err=>{
+    console.log(err);
+})
--- models/product.js
+++ models/product.js
@@ -1,33 +1,26 @@
-const db= require('../util/database');
-const Cart = require('./cart');
+const Sequelize=require('sequelize');
+const sequelize=require('../util/database');
 
+const Product=sequelize.define('product',{
+id:{
+  type:Sequelize.INTEGER,
+  autoIncrement:true,
+  allowNull:false,
+  primaryKey:true
+},
+title:Sequelize.STRING,
+price:{
+  type:Sequelize.DOUBLE,
+  allowNull:false
+},
+imageUrl:{
+  type:Sequelize.STRING,
+  allowNull:false
+},
+description:{
+  type:Sequelize.STRING,
+  allowNull:false
+}
+})
 
-
-
-
-module.exports = class Product {
-  constructor(id, title, imageUrl, description, price) {
-    this.id = id;
-    this.title = title;
-    this.imageUrl = imageUrl;
-    this.description = description;
-    this.price = price;
-  }
-
-  save() {
-    return db.execute('INSERT into products(title,description,price,imageUrl) values(?,?,?,?)',
-    [this.title,this.description,this.price,this.imageUrl]);
-  }
-
-  static deleteById(id) {
-   
-  }
-
-  static fetchAll() {
-  return db.execute('Select * from products');
-  }
-
-  static findById(id) {
-  return db.execute('Select * from products where products.id=?',[id]);
-  }
-};
+module.exports=Product;
